bf673c17b06bbf2c4ad3dce34f106060e79f4cc9
Chris <chris@darkside.local>
2021-11-08 08:12:08 -0800

    clarify init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 cd ~/.vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PluginInstall +qall
